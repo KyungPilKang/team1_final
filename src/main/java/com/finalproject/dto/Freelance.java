d28cb8a186e2d6ca32e6eddc30e3aa4301d6bc32
KyungPilKang <97011445+KyungPilKang@users.noreply.github.com>
2022-03-28 13:24:54 +0900

    KKP/추가 : Freelance DTO 최초 구현

---
+++ src/main/java/com/finalproject/dto/Freelance.java
@@ -0,0 +1,45 @@
+package com.finalproject.dto;
+
+import java.io.File;
+
+public class Freelance {
+	private int no;
+	private String member_no;
+	private String name;
+	private String region;
+	private String gender;
+	private String age;
+	private String age_section;
+	private String email;
+	private String phone;
+	private String freelance_type;
+	private String labor_type;
+	private String labor_time;
+	private String carrer;
+	private String grade;
+	private String class_type;
+	private String class_type_etc;
+	private String univ;
+	private String major;
+	private File certification_file;
+	private String certification_file_name;
+	private String camera;
+	private String camera_etc;
+	private String lecture_video;
+	private String lecture_video_etc;
+	private String lecture_video_format;
+	private String editor_tool;
+	private String editor_tool_etc;
+	private String lecture_edit;
+	private String lecture_edit_etc;
+	private String lecture_edit_format;
+	private String introduce_title;
+	private String introduce_contents;
+	private File promotion_video_file;
+	private String promotion_video_file_name;
+	private File banner_image_file;
+	private String banner_image_file_name;
+	private String month_pay;
+	private String week_pay;
+	private String case_pay;
+}
