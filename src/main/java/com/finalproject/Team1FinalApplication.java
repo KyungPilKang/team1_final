6a861336e44ea9242ae6bdafe5100623339ac16b
KyungPilKang <97011445+KyungPilKang@users.noreply.github.com>
2022-03-28 09:11:35 +0900

    Update Team1FinalApplication.java

--- src/main/java/com/finalproject/Team1FinalApplication.java
+++ src/main/java/com/finalproject/Team1FinalApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Team1FinalApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Team1FinalApplication.class, args);
 	}
