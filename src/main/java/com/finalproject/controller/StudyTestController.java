d3cb25a4af9b3d2fc3935caee6b39de9445ff48d
Eleen1379 <95533531+Eleen1379@users.noreply.github.com>
2022-03-29 18:15:45 +0900

    KMJ/추가 : studyReg.jsp 등록페이지 추가 (datepicker, timepicker 가 작동이 안되네요) KMJ/변경 : StudyTestController testfile 폴더 아래로 경로명 변경하였습니다.

--- src/main/java/com/finalproject/controller/StudyTestController.java
+++ src/main/java/com/finalproject/controller/StudyTestController.java
@@ -2,54 +2,67 @@ package com.finalproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StudyTestController {
 	
 	@GetMapping("datepickertest")
 	public String datepickertest() {
-		return "datepickertest";
+		return "testfile/datepickertest";
 	}
 		
 	@GetMapping("timepicker")
 	public String timepicker() {
-		return "timepicker";
+		return "testfile/timepicker";
 		
 	}
 	
 	@GetMapping("checkboxtest")
 	public String checkboxtest() {
-		return "checkboxtest";
+		return "testfile/checkboxtest";
 		
 	}
 	
 	@GetMapping("datalist")
 	public String datalist() {
 		System.out.println("값 넘어옴");
-		return "checkboxtest";
+		return "testfile/checkboxtest";
 		
 	}
 	
 	@GetMapping("dropdowntest")
 	public String dropdowntest() {
-		return "dropdowntest";
+		return "testfile/dropdowntest";
 		
 	}
 	@GetMapping("dropdownlist")
 	public String dropdownlist() {
 		System.out.println("값 넘어옴");
-		return "dropdowntest";
+		return "testfile/dropdowntest";
 		
 	}
 	
 	@GetMapping("aligntest")
 	public String aligntest() {
-		return "aligntest";
+		return "testfile/aligntest";
 	}
 	
 	@GetMapping("modaltest")
 	public String modaltest() {
-		return "modaltest";
+		return "testfile/modaltest";
 	}
 
+	@RequestMapping("/studyReg")
+	public String studyReg() {
+		return "testfile/studyReg";
+	}
+	
+	@GetMapping("testpage")
+	public String testpage() {
+		return "testfile/testpage";
+	}
+	
+	
+	
 }
