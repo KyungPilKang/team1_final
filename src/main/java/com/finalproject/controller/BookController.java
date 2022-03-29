0a1263585d525db947fbfceed592e96e841f22bf
jayPark14 <100342241+jayPark14@users.noreply.github.com>
2022-03-29 23:29:06 +0900

    :lipstick: PJS/추가 : bookstore 화면 제작

--- src/main/java/com/finalproject/controller/BookController.java
+++ src/main/java/com/finalproject/controller/BookController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BookController {
 
-
-
     @GetMapping("/book")
     public String test() {
         return "/bookstore/bookStore";
