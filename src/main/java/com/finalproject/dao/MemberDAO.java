d39b835ad4b735f88f6402bb8af8a2862a716a1f
KyungPilKang <97011445+KyungPilKang@users.noreply.github.com>
2022-03-30 11:24:15 +0900

    KKP/추가 : 프리랜서 등록 서비스 1차 구현

--- src/main/java/com/finalproject/dao/MemberDAO.java
+++ src/main/java/com/finalproject/dao/MemberDAO.java
@@ -1,7 +1,12 @@
 package com.finalproject.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
 import com.finalproject.dto.Member;
 
+@Mapper
+@Repository
 public interface MemberDAO {
 	
 	public Member selectMemberByUsername(String username) throws Exception;
---
+++ src/main/java/com/finalproject/service/FreelanceServiceImpl.java
@@ -0,0 +1,16 @@
+package com.finalproject.service;
+
+import org.springframework.stereotype.Service;
+
+import com.finalproject.dto.Freelance;
+
+@Service
+public class FreelanceServiceImpl implements FreelanceService {
+
+	@Override
+	public void regFreelance(Freelance free) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
