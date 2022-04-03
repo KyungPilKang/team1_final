5b77b70d515b93f10868c37b8d437da3d228c027
jayPark14 <100342241+jayPark14@users.noreply.github.com>
2022-04-03 14:39:00 +0900

    PJS/ pull failed 해결 commit

--- src/main/java/com/finalproject/controller/BookController.java
+++ src/main/java/com/finalproject/controller/BookController.java
@@ -3,8 +3,6 @@ package com.finalproject.controller;
 import com.finalproject.dto.Book;
 import com.finalproject.dto.PageInfo;
 import com.finalproject.service.BookStoreService;
-import org.apache.tomcat.util.codec.binary.Base64;
-import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +10,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletContext;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.util.List;
 
 @Controller
