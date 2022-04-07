b4b70a8c674f2ece7d4272c82a528ae2ae6ba9fc
KyungPilKang <97011445+KyungPilKang@users.noreply.github.com>
2022-04-07 10:49:34 +0900

    KKP/수정 : stduyService DI 오류 수정, study.xml 오류 수정

--- src/main/java/com/finalproject/service/StudyService.java
+++ src/main/java/com/finalproject/service/StudyService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import com.finalproject.dto.Study;
 import com.finalproject.dto.StudyTeam;
 
-@Service
 public interface StudyService {
 	//스터디 등록 
 	public void regStudy(Study study) throws Exception;
